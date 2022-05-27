d63a8fcec8afcf85ecee125e6f4bc48fe2be7db7
nrenner <nrenner0211@gmail.com>
2022-05-27 01:09:31 -0400

    basic html, css, js added

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// global variables
+var startBtn = document.querySelector(".start-btn");
+var timer = document.querySelector(".timer");
+var timeLeft = document.querySelector(".time-left");
+var timeSec = document.querySelector(".time-sec");
+
+
+// dom reference
+console.dir(window.document);
+
+startBtn.addEventListener("click", function (event) {
+
+})
+
+// // start timer event listener
+// startBtn.addEventListener("click", function (event) {
+// var time = 15;
+// var myInterval;
+
+//     // stops timer from starting with every click
+//     clearInterval(myInterval);
+
+//     // starts timer on click
+//     myInterval = setInterval(function () {
+//         time --;
+//         timer.innerHTML = time;
+//     }, 1000);
+
+//     if (time < 0) {
+//         alert("You lose!");
+//     }
+// });
